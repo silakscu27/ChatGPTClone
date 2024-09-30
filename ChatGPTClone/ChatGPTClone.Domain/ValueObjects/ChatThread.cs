e0e578ccd290a781428ea84c4f5857890bda1ed2
Sila Kuscu <silaksc27@gmail.com>
2024-09-30 20:58:53 +0300

    Create ValueObjects folder

---
+++ ChatGPTClone/ChatGPTClone.Domain/ValueObjects/ChatThread.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatGPTClone.Domain.ValueObjects;
+
+public sealed class ChatThread
+{
+    public string Id { get; set; }
+
+    public List<ChatMessage> Messages { get; set; } = [];
+
+    public DateTimeOffset CreatedOn { get; set; }
+}
