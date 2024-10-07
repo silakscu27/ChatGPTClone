b8d3b5c317e92beee4167ff3d24963608205533d
Sila Kuscu <silaksc27@gmail.com>
2024-10-07 10:34:58 +0300

    Add Identity folder under Infrastructure

---
+++ ChatGPTClone/ChatGPTClone.Infrastructure/Identity/AppUserClaim.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatGPTClone.Infrastructure.Identity
+{
+    public class AppUserClaim : IdentityUserClaim<Guid>
+    {
+
+    }
+}
---
+++ ChatGPTClone/ChatGPTClone.Infrastructure/Identity/RoleClaim.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace ChatGPTClone.Infrastructure.Identity
+{
+    public class RoleClaim : IdentityRoleClaim<Guid>
+    {
+    }
+}
