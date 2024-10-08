087bd64018ed2522182fb4ea69e06500e10aede6
Sila Kuscu <silaksc27@gmail.com>
2024-10-08 16:06:05 +0300

    Update ChatSessionCreateCommandValidator

--- ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandValidator.cs
+++ ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandValidator.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using FluentValidation;
 namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Create
 {
-    public class ChatSessionCreateCommandValidator
+    public class ChatSessionCreateCommandValidator : AbstractValidator<ChatSessionCreateCommand>
     {
+        public ChatSessionCreateCommandValidator() 
+        {
+            RuleFor(x => x.Model)
+                .NotNull()
+                .WithMessage("Model is required")
+                .NotEmpty()
+                .WithMessage("Model is required")
+                .IsInEnum()
+                .WithMessage("Model is invalid");
+
+            RuleFor(x => x.Content)
+                .NotNull()
+                .WithMessage("Content is required")
+                .NotEmpty()
+                .WithMessage("Content is required")
+                .MaximumLength(4000)
+                .WithMessage("Content must not exceed 4000 characters");
+        }
     }
 }
