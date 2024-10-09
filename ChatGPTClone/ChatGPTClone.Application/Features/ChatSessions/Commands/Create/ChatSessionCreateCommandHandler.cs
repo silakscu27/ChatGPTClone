4ea2ac12c8b7b122d219f17d042176775d70aa45
Sila Kuscu <silaksc27@gmail.com>
2024-10-09 16:42:42 +0300

    Update CreateCommandHandler

--- ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandHandler.cs
+++ ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Commands/Create/ChatSessionCreateCommandHandler.cs
@@ -1,21 +1,28 @@
 ﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.General;
+using ChatGPTClone.Application.Features.ChatSessions.Queries.GetAll;
+using ChatGPTClone.Application.Features.ChatSessions.Queries.GetById;
+using ChatGPTClone.Domain.Entities;
+using ChatGPTClone.Domain.Enums;
+using ChatGPTClone.Domain.ValueObjects;
 using MediatR;
 
 namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Create
 {
-    public class ChatSessionCreateCommandHandler : IRequestHandler<ChatSessionCreateCommand, Guid>
+    public class ChatSessionCreateCommandHandler : IRequestHandler<ChatSessionCreateCommand, ResponseDto<Guid>>
     {
         private readonly IApplicationDbContext _context;
         private readonly ICurrentUserService _currentUserService;
 
-       public ChatSessionCreateCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService)
+        public ChatSessionCreateCommandHandler(IApplicationDbContext context, ICurrentUserService currentUserService)
         {
             _context = context;
-            _currentUserService = currentUserService; 
+
+            _currentUserService = currentUserService;
+
         }
 
-
-        public async Task<Guid> Handle(ChatSessionCreateCommand request, CancellationToken cancellationToken)
+        public async Task<ResponseDto<Guid>> Handle(ChatSessionCreateCommand request, CancellationToken cancellationToken)
         {
             var chatSession = request.ToChatSession(_currentUserService.UserId);
 
@@ -24,8 +31,21 @@ namespace ChatGPTClone.Application.Features.ChatSessions.Commands.Create
             _context.ChatSessions.Add(chatSession);
 
             await _context.SaveChangesAsync(cancellationToken);
-           
-            return chatSession.Id;
+
+            return new ResponseDto<Guid>(chatSession.Id, "A new chat session was created successfully.");
+        }
+
+       
+        private ChatMessage CreateAssistantChatMessage(string response, GptModelType model)
+        {
+            return new ChatMessage()
+            {
+                Id = Ulid.NewUlid().ToString(),
+                Model = model,
+                Type = ChatMessageType.Assistant,
+                Content = response,
+                CreatedOn = DateTimeOffset.UtcNow,
+            };
         }
     }
 }
