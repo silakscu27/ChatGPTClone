b7d0e3b8208608af1611141387c3f1fdb583ca40
Sila Kuscu <silaksc27@gmail.com>
2024-10-11 18:50:34 +0300

    Create DependencyInjection class under Application layer

---
+++ ChatGPTClone/ChatGPTClone.Application/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using ChatGPTClone.Application.Behaviors;
+
+namespace ChatGPTClone.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+                // Validation PipeLine
+                config.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            });
+
+            return services;
+        }
+    }
+}
