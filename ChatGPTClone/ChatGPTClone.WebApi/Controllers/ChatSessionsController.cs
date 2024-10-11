63f8723aef6b8271117f464cc20c389cf7f3e371
Sila Kuscu <silaksc27@gmail.com>
2024-10-11 18:48:30 +0300

    Added controllers classes

---
+++ ChatGPTClone/ChatGPTClone.WebApi/Controllers/ChatSessionsController.cs
@@ -0,0 +1,38 @@
+﻿using ChatGPTClone.Application.Features.ChatSessions.Commands.Create;
+using ChatGPTClone.Application.Features.ChatSessions.Queries.GetAll;
+using ChatGPTClone.Application.Features.ChatSessions.Queries.GetById;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace ChatGPTClone.WebApi.Controllers;
+
+[Authorize]
+public class ChatSessionsController : ApiControllerBase
+{
+    public ChatSessionsController(ISender mediatR) : base(mediatR)
+    {
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+    {
+        return Ok(await MediatR.Send(new ChatSessionGetAllQuery(), cancellationToken));
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return Ok(await MediatR.Send(new ChatSessionGetByIdQuery(id), cancellationToken));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(ChatSessionCreateCommand command, CancellationToken cancellationToken)
+    {
+        return Ok(await MediatR.Send(command, cancellationToken));
+    }
+
+    
+
+}
