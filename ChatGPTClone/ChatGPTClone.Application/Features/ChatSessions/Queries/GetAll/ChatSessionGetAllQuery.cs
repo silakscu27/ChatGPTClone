ccff5dc000b3eb428efee8906e56e5e1a7364017
Sila Kuscu <silaksc27@gmail.com>
2024-10-11 18:53:41 +0300

    Create GetAll Queries files

--- ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Queries/GetAll/ChatSessionGetAllQuery.cs
+++ ChatGPTClone/ChatGPTClone.Application/Features/ChatSessions/Queries/GetAll/ChatSessionGetAllQuery.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
 namespace ChatGPTClone.Application.Features.ChatSessions.Queries.GetAll
 {
-    public class ChatSessionGetAllQuery
+    public class ChatSessionGetAllQuery : IRequest<List<ChatSessionGetAllDto>>
     {
+        public ChatSessionGetAllQuery()
+        {
 
-     
+        }
     }
 }
