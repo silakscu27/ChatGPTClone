4a49a9c6bd54ff1aedf158ed55bf4bef7bef8f28
Sila Kuscu <silaksc27@gmail.com>
2024-10-11 18:49:40 +0300

    Update program.cs

--- ChatGPTClone/ChatGPTClone.WebApi/Program.cs
+++ ChatGPTClone/ChatGPTClone.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using ChatGPTClone.Application;
+using ChatGPTClone.Infrastructure;
+using ChatGPTClone.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+
 builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddWebApi(builder.Configuration);
 
 var app = builder.Build();
 
@@ -19,6 +28,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
