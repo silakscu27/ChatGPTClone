d6cac0410e1534e1edf989d1e0d2424c08f2f69d
Sila Kuscu <silaksc27@gmail.com>
2024-10-14 20:48:20 +0300

    Update ValidationBehaaviour

--- ChatGPTClone/ChatGPTClone.Application/Common/Behaviours/ValidationBehaviour.cs
+++ ChatGPTClone/ChatGPTClone.Application/Common/Behaviours/ValidationBehaviour.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
 
-namespace ChatGPTClone.Application.Common.Behaviours
+namespace ChatGPTClone.Application.Common.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
 {
-    public class ValidationBehaviour
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    // Doğrulayıcıları dependency injection yoluyla al
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
     {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        // Doğrulayıcı yoksa sonraki işleme geç
+        if (!_validators.Any())
+            return await next();
+
+        var context = new ValidationContext<TRequest>(request);
+
+        // Tüm doğrulayıcıları paralel olarak çalıştır
+        var validationTasks = _validators.Select(v => v.ValidateAsync(context, cancellationToken));
+
+        var validationResults = await Task.WhenAll(validationTasks);
+
+        // Hataları topla
+        var failures = validationResults
+            .Where(r => r.Errors.Any())
+            .SelectMany(r => r.Errors)
+            .ToArray();
+
+        // Hata varsa exception fırlat
+        if (failures.Any())
+            throw new ValidationException(failures);
+
+        // Hata yoksa sonraki işleme geç
+        return await next();
     }
 }
