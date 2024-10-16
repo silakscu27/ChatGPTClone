9c1fad1d9b4e36052bec728bd36925879500a069
Sila Kuscu <silaksc27@gmail.com>
2024-10-16 13:37:04 +0300

    Create GlobalExceptionFilter class

---
+++ ChatGPTClone/ChatGPTClone.WebApi/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,56 @@
+﻿using ChatGPTClone.Application.Common.Models.Errors;
+using ChatGPTClone.Application.Common.Models.General;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.ComponentModel.DataAnnotations;
+namespace ChatGPTClone.WebApi.Filters;
+
+public class GlobalExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger<GlobalExceptionFilter> _logger;
+
+    public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        _logger.LogError(context.Exception, context.Exception.Message);
+        context.ExceptionHandled = true;
+
+        if (context.Exception is FluentValidation.ValidationException) 
+        {
+            var exception = context.Exception as FluentValidation.ValidationException;
+            var responseMessage = "one or more validation errors occured";
+            List<ErrorDto> errors = new();
+
+            var propertyNames = exception.Errors
+                .Select(error => error.ErrorMessage)
+                .Distinct();
+
+            foreach (var propertyName in propertyNames)
+            {
+                var messages = exception.Errors
+                    .Where(e => e.PropertyName == propertyName)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                errors.Add(new ErrorDto(propertyName, messages));
+            }
+            context.Result = new BadRequestObjectResult(new ResponseDto<string>(responseMessage, errors))
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+        }
+        else
+        {
+            context.Result = new ObjectResult(new ResponseDto<string>("internal server error", false))
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+    }
+  }
