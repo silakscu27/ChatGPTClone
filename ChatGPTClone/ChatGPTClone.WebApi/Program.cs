5ec42b5da70a6d770a62daf99783882a4a193c7c
Sila Kuscu <silaksc27@gmail.com>
2024-10-16 13:37:46 +0300

    Update Program class

--- ChatGPTClone/ChatGPTClone.WebApi/Program.cs
+++ ChatGPTClone/ChatGPTClone.WebApi/Program.cs
@@ -1,38 +1,81 @@
 using ChatGPTClone.Application;
 using ChatGPTClone.Infrastructure;
 using ChatGPTClone.WebApi;
+using ChatGPTClone.WebApi.Filters;
+using Microsoft.Extensions.Options;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .MinimumLevel.Warning()
+    .CreateLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddApplication();
-
-builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddWebApi(builder.Configuration);
-
-var app = builder.Build();
-
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Warning("Starting web application");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog();
+
+    // Add services to the container.
+
+    // Controller'larý servis olarak ekle
+    builder.Services.AddControllers(opt =>
+    {
+        // Global exception filtresini ekle
+        opt.Filters.Add<GlobalExceptionFilter>();
+    });
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddApplication();
+
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddWebApi(builder.Configuration, builder.Environment);
+
+    var app = builder.Build();
+
+    app.UseCors("AllowAll");
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    var requestLocalizationOptions = app.Services
+        .GetRequiredService<IOptions<RequestLocalizationOptions>>().Value;
+
+    app.UseRequestLocalization(requestLocalizationOptions);
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+Func<IServiceProvider, object> EnvironmentManager(string webRootPath)
+{
+    throw new NotImplementedException();
+}
