04c473922e1407bc28c9fdf5e1dcc9dbc529d87c
Sila Kuscu <silaksc27@gmail.com>
2024-10-17 15:43:30 +0300

    Create Localization folder

---
+++ ChatGPTClone/ChatGPTClone.Application/Localization/CommonLocalizationKeys.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ChatGPTClone.Application.Localization
+{
+    public class CommonLocalizationKeys
+    {
+        public static string GeneralValidationException => nameof(GeneralValidationException);
+        public static string GeneralNotFoundException => nameof(GeneralNotFoundException);
+        public static string GeneralInternalServerException => nameof(GeneralInternalServerException);
+        public static string ValidationIsRequired => nameof(ValidationIsRequired);
+        public static string ValidationIsInvalid => nameof(ValidationIsInvalid);
+        public static string ValidationMustBeBetween => nameof(ValidationMustBeBetween);
+    }
+}
