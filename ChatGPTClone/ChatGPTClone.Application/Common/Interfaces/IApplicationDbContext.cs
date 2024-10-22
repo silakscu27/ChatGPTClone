64ffcc162e7fe56356927f03e1864f5f3690d0e0
Sila Kuscu <silaksc27@gmail.com>
2024-10-22 15:51:08 +0300

    Updat IApplicationDbContext

--- ChatGPTClone/ChatGPTClone.Application/Common/Interfaces/IApplicationDbContext.cs
+++ ChatGPTClone/ChatGPTClone.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,13 +1,12 @@
 ﻿using ChatGPTClone.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 
 namespace ChatGPTClone.Application.Common.Interfaces
 {
     public interface IApplicationDbContext
     {
         DbSet<ChatSession> ChatSessions { get; set; }
-        DbSet<RefreshToken> RefreshTokens { get; set; }
+        //DbSet<RefreshToken> RefreshTokens { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 
