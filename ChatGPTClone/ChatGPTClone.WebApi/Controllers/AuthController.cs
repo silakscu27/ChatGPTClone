b3cccfe3936856be6256d8de7dca8c4de413e9f0
Sila Kuscu <silaksc27@gmail.com>
2024-10-22 14:35:18 +0300

    Create AuthController

---
+++ ChatGPTClone/ChatGPTClone.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ChatGPTClone.Application.Features.Auth.Commands.Login;
+
+namespace ChatGPTClone.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ApiControllerBase
+    {
+        public AuthController (ISender mediatr) : base(mediatr) 
+        { 
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(AuthLoginCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await MediatR.Send(command , cancellationToken));
+        }
+    }
+}
