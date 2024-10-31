1a2720db74932003330d8d4f0877c462e493acc3
Sila Kuscu <silaksc27@gmail.com>
2024-10-31 20:51:37 +0300

    Update AuthVerifyEmailCommandValidator

--- ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandValidator.cs
+++ ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandValidator.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ChatGPTClone.Application.Common.Interfaces;
+using FluentValidation;
 
-namespace ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+namespace ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail;
+
+public class AuthVerifyEmailCommandValidator : AbstractValidator<AuthVerifyEmailCommand>
 {
-    internal class AuthVerifyEmailCommandValidator
+    private readonly IIdentityService _identityService;
+
+    public AuthVerifyEmailCommandValidator(IIdentityService identityService)
     {
+        _identityService = identityService;
+
+        RuleFor(x => x.Email)
+        .NotEmpty()
+        .EmailAddress()
+        .MustAsync(EmailExists)
+        .WithMessage("Email not found.");
+
+        RuleFor(x => x.Token)
+        .NotEmpty()
+        .MinimumLength(20)
+        .WithMessage("Token is invalid.");
+
+    }
+
+    private Task<bool> EmailExists(string email, CancellationToken cancellationToken)
+    {
+        return _identityService.CheckEmailExistsAsync(email, cancellationToken);
     }
 }
