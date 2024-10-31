9aac28d1c6a37ba5fb2cfec8d3fbef3bcf4ec1e5
Sila Kuscu <silaksc27@gmail.com>
2024-10-31 20:51:21 +0300

    Update AuthVerifyEmailCommandHandler

--- ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandHandler.cs
+++ ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/VerifyEmail/AuthVerifyEmailCommandHandler.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Common.Models.General;
+using ChatGPTClone.Application.Common.Models.Identity;
+using MediatR;
 
-namespace ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail
+namespace ChatGPTClone.Application.Features.Auth.Commands.VerifyEmail;
+
+public class AuthVerifyEmailCommandHandler : IRequestHandler<AuthVerifyEmailCommand, ResponseDto<string>>
 {
-    internal class AuthVerifyEmailCommandHandler
+    private readonly IIdentityService _identityService;
+
+    public AuthVerifyEmailCommandHandler(IIdentityService identityService)
     {
+        _identityService = identityService;
+    }
+
+    public async Task<ResponseDto<string>> Handle(AuthVerifyEmailCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _identityService.VerifyEmailAsync(new IdentityVerifyEmailRequest(request.Email, request.Token), cancellationToken);
+
+        return new ResponseDto<string>(data: response.Email, message: "Email verified successfully.");
     }
 }
