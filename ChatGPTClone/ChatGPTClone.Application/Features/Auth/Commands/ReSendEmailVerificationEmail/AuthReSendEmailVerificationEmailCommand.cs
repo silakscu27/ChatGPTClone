fb1a5126af19d879b088e1b19d67cc989cfb26dd
Sila Kuscu <silaksc27@gmail.com>
2024-11-01 10:50:54 +0300

    Update AuthReSendEmailVerificationEmailCommand

--- ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/ReSendEmailVerificationEmail/AuthReSendEmailVerificationEmailCommand.cs
+++ ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/ReSendEmailVerificationEmail/AuthReSendEmailVerificationEmailCommand.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ChatGPTClone.Application.Common.Models.General;
+using MediatR;
 
-namespace ChatGPTClone.Application.Features.Auth.Commands.ReSendEmailVerificationEmail
+namespace ChatGPTClone.Application.Features.Auth.Commands.ReSendEmailVerificationEmail;
+
+public class AuthReSendEmailVerificationEmailCommand : IRequest<ResponseDto<string>>
 {
-    internal class AuthReSendEmailVerificationEmailCommand
+    public string Email { get; set; }
+
+    public AuthReSendEmailVerificationEmailCommand(string email)
     {
+        Email = email;
     }
+
 }
