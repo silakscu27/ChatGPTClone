4d048825724d4fc74162c39d288470554ba0e5b4
Sila Kuscu <silaksc27@gmail.com>
2024-11-01 10:51:34 +0300

    Update AuthReSendEmailVerificationEmailCommandValidator

--- ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/ReSendEmailVerificationEmail/AuthReSendEmailVerificationEmailCommandValidator.cs
+++ ChatGPTClone/ChatGPTClone.Application/Features/Auth/Commands/ReSendEmailVerificationEmail/AuthReSendEmailVerificationEmailCommandValidator.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ChatGPTClone.Application.Common.Interfaces;
+using FluentValidation;
+using FluentValidation.Validators;
 
-namespace ChatGPTClone.Application.Features.Auth.Commands.ReSendEmailVerificationEmail
+namespace ChatGPTClone.Application.Features.Auth.Commands.ReSendEmailVerificationEmail;
+
+public class AuthReSendEmailVerificationEmailCommandValidator : AbstractValidator<AuthReSendEmailVerificationEmailCommand>
 {
-    internal class AuthReSendEmailVerificationEmailCommandValidator
+    private readonly IIdentityService _identityService;
+    public AuthReSendEmailVerificationEmailCommandValidator(IIdentityService identityService)
     {
+        _identityService = identityService;
+
+        RuleFor(x => x.Email)
+        .NotEmpty()
+        .EmailAddress()
+        .MustAsync(CheckEmailExistsAsync)
+        .WithMessage("Email address is not exists.")
+        .MustAsync(CheckIfEmailVerifiedAsync)
+        .WithMessage("Email address is already verified.");
+    }
+
+    private Task<bool> CheckEmailExistsAsync(string email, CancellationToken cancellationToken)
+    {
+        return _identityService.CheckEmailExistsAsync(email, cancellationToken);
+    }
+
+    private async Task<bool> CheckIfEmailVerifiedAsync(string email, CancellationToken cancellationToken)
+    {
+        return !await _identityService.CheckIfEmailVerifiedAsync(email, cancellationToken);
     }
 }
