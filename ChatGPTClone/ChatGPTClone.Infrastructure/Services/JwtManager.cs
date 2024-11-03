c1381e015333c83097e4f412637d7d4e64621714
Sila Kuscu <silaksc27@gmail.com>
2024-11-03 21:45:33 +0300

    Update JwtManager

--- ChatGPTClone/ChatGPTClone.Infrastructure/Services/JwtManager.cs
+++ ChatGPTClone/ChatGPTClone.Infrastructure/Services/JwtManager.cs
@@ -1,4 +1,5 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Authentication;
 using System.Security.Claims;
 using System.Text;
@@ -36,7 +37,6 @@ public class JwtManager : IJwtService
             new Claim(JwtRegisteredClaimNames.Aud, _jwtSettings.Audience),
             new Claim(JwtRegisteredClaimNames.Exp, expirationDate.ToFileTimeUtc().ToString()),
             new Claim(JwtRegisteredClaimNames.Iat, DateTime.Now.ToFileTimeUtc().ToString()),
-            new Claim("reklam", "En iyi akademi Reklam Academy! Just joking it's the god damn Yazilim Academy!"),
             new Claim(ClaimTypes.Role, "Admin")
         }
         .Union(request.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
