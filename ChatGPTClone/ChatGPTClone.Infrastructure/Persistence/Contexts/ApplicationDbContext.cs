6c4f4b5f84cd54ca8e29e50ce19dd28adb7bc86d
Sila Kuscu <silaksc27@gmail.com>
2024-11-05 18:39:19 +0300

    Update Configuration pages

--- ChatGPTClone/ChatGPTClone.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
+++ ChatGPTClone/ChatGPTClone.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -1,14 +1,16 @@
-﻿using ChatGPTClone.Domain.Entities;
-using ChatGPTClone.Domain.Identity;
+﻿using ChatGPTClone.Application.Common.Interfaces;
+using ChatGPTClone.Application.Features.Auth.Commands.RefreshToken;
+using ChatGPTClone.Domain.Entities;
 using ChatGPTClone.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ChatGPTClone.Infrastructure.Persistence.Contexts;
 
-public class ApplicationDbContext : IdentityDbContext<AppUser, Role, Guid, AppUserClaim, AppUserRole, AppUserLogin, RoleClaim, AppUserToken>
+public class ApplicationDbContext : IdentityDbContext<AppUser, Role, Guid, AppUserClaim, AppUserRole, AppUserLogin, RoleClaim, AppUserToken>, IApplicationDbContext
 {
     public DbSet<ChatSession> ChatSessions { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
